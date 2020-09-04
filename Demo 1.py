961fab158086c01df8b2cde81490f246bc8f988c
illuruvineelreddy <70740939+illuruvineelreddy@users.noreply.github.com>
2020-09-04 11:49:38 +0530

    Add files via upload

---
+++ Demo 1.py
@@ -0,0 +1,86 @@
+
+# coding: utf-8
+
+# In[6]:
+
+
+print("hello world")
+
+
+# # int , float 
+
+# In[5]:
+
+
+type(4)
+
+
+# In[6]:
+
+
+type(7.7)
+
+
+# # varables and data types
+
+# In[7]:
+
+
+x=7
+
+
+# In[8]:
+
+
+x
+
+
+# Memory Location
+
+# In[9]:
+
+
+id(x)
+
+
+# In[11]:
+
+
+x=8
+
+
+# In[12]:
+
+
+id(x)
+
+
+# In[13]:
+
+
+x=7
+
+
+# In[14]:
+
+
+id(x)
+
+
+# In[15]:
+
+
+x='vineel reddy'
+
+
+# In[16]:
+
+
+x
+
+
+# In[17]:
+
+
+id(x)
+
